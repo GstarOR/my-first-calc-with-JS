0e15bee4c0e9fa190ab71b22ee037db6259cbcb2
GstarOR <godswillavwighomerhe@gmail.com>
2025-10-23 10:48:37 +0100

    God knows all

--- learning/weather1/weather.js
+++ learning/weather1/weather.js
@@ -1,12 +1,19 @@
 const apiKey = "2f6026032cb61262d6aef60ed8ccbfe5";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99";
-
-async function checkWeather() {
-    const response = await fetch(apiUrl + `&appid=${apiKey}`);
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const searchBox = document.querySelector('.search input');
+const searchSpan = document.querySelector('.search span');
+async function checkWeather(location) {
+    const response = await fetch(apiUrl + location + `&appid=${apiKey}`);
     var data = await response.json();
 
     console.log(data);
-
+//now we will display the data in every needed space with respect to our api object display
+    document.querySelector('.location').innerHTML = data.name;
+    document.querySelector('.temperature span').innerHTML = Math.round(data.main.temp) + "&#176;C";
+    document.querySelector('.humi').innerHTML = data.main.humidity + "%";
+    document.querySelector('.wind').innerHTML = data.wind.speed + " km/h";
 }
 
-checkWeather();
+searchSpan.addEventListener('click', ()=>{
+    checkWeather(searchBox.value);
+});
